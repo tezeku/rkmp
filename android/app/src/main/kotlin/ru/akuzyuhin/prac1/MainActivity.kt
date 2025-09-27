1d743efd6237f3d1346689d4b879c8f7aed9445a
Art <cuzyuhin@yandex.ru>
2025-09-27 09:55:08 +0300

    add my info on main screen

---
+++ android/app/src/main/kotlin/ru/akuzyuhin/prac1/MainActivity.kt
@@ -0,0 +1,5 @@
+package ru.akuzyuhin.prac1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child:
+            const Text(
+                'ФИО: Кузюхин Артемий Вячеславович\n'
+                'Номер группы: ИКБО-06-22\n'
+                'Номер студенческого билета: 22И0289',
+              textAlign: TextAlign.center,
+            ),
+      ),
+    );
+  }
+}
