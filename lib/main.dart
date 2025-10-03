0b30be0dfd6b494545ec00f062e72a92b755931e
Art <cuzyuhin@yandex.ru>
2025-10-03 16:21:23 +0300

    Add style for text on main screen

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 'Номер группы: ИКБО-06-22\n'
                 'Номер студенческого билета: 22И0289',
               textAlign: TextAlign.center,
-            ),//
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.blueGrey
+              )
+            ),
       ),
     );
   }
