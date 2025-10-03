cb89b99a060ce41d0c101acb92948e617b151603
Art <cuzyuhin@yandex.ru>
2025-10-03 17:43:46 +0300

    Replace a Column Widget with Row Widget on main screen

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
