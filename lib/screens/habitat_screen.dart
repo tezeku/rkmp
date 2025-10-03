596b0c48e210577a96ad322209f5951409226083
Art <cuzyuhin@yandex.ru>
2025-10-04 00:09:44 +0300

    реализация habitat_screen

--- lib/screens/habitat_screen.dart
+++ lib/screens/habitat_screen.dart
@@ -1,3 +1,27 @@
-class HabitatScreen {
-  const HabitatScreen();
+import 'package:flutter/material.dart';
+
+class HabitatScreen extends StatelessWidget {
+  const HabitatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Где живут пингвины:',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 15),
+          Text('• Антарктида и субантарктические острова'),
+          Text('• Южная Америка (Аргентина, Чили)'),
+          Text('• Южная Африка'),
+          Text('• Австралия и Новая Зеландия'),
+          Text('• Галапагосские острова'),
+        ],
+      ),
+    );
+  }
 }
