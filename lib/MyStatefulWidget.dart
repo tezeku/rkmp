af84c3627e077ac17de6161ed0198510791ce848
Art <cuzyuhin@yandex.ru>
2025-10-03 23:18:54 +0300

    add MyStatefulWidget

---
+++ lib/MyStatefulWidget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Mystatefulwidget extends StatefulWidget {
+  const Mystatefulwidget({super.key});
+
+  @override
+  State<Mystatefulwidget> createState() => _MystatefulwidgetState();
+}
+
+class _MystatefulwidgetState extends State<Mystatefulwidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
